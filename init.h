043e362c30903a3fce6fc1455d495199e03e2299
AlexeyEf <avefimov2013@gmail.com>
2017-04-25 11:36:00 +0300

    Add files via upload

---
+++ init.h
@@ -0,0 +1,13 @@
+
+#include <stm32l1xx.h>
+#include <system_stm32l1xx.h>
+#include <stm32l1xx_gpio.h>
+#include <stm32l1xx_tim.h>
+#include <stm32l1xx_rcc.h>
+
+
+//function of configuration of ports timer module
+void gpio_init(void);
+void tim_init(void);
+void rcc_init(void);
+
